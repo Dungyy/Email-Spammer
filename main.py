4fa23a0b693f9acff4fc8f083c2e8b090b0ce9e5
Erick Munoz <88725477+Dungyy@users.noreply.github.com>
2022-07-20 02:28:10 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import smtplib
 import time
-for i in range(20):
+for i in range(3):
     time.sleep(.5)
 # range=how many times / sleep=is in how long before sends another
     server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
 
-    server.login("#enter your email", "#enter your password")
-    server.sendmail("#enter your email",
-                    "#enter the email your sending too",
-                    "#enter the message you wanna send")
+    server.login("hacker.dungy@gmail.com", "dungyM0217!")
+    server.sendmail("dungy_hacker@gmail.com",
+                    "erick_m-17@hotmail.com",
+                    "YOURE ACCOUNT IS BEING HACKED\n""HAHAHAHAHA\n""GOOD GAME")
     server.quit()
 
 # made by dungy
